9c3177c337b1c9a1a2fd24da7aaab2477849fc69
RomanNikitenko03 <rnikitenko953@mail.com>
2024-10-08 12:58:14 +0300

    FirstTask

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(176, 198, 165),
+            Color.FLORALWHITE,
+            Color.MAGENTA,
+            Color.NAVY,
+            Color.rgb(198, 164, 0),
+            Color.LAVENDER,
+            Color.rgb(87, 194, 0)
         };
         
         /**
